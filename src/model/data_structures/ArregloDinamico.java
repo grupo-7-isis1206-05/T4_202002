9f909192f07dd8dbd7a8b394ffc1f94e0e57ad46
NotABot32 <a.alarconc@uniandes.edu.co>
2020-08-25 22:42:52 -0500

    arregolos al arreglo

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -21,7 +21,7 @@ public class ArregloDinamico<T extends Comparable<T>> implements IArregloDinamic
 	/**
 	 * Arreglo de elementos de tamaNo maximo
 	 */
-	private String elementos[ ];
+	private T elementos[ ];
 
 	/**
 	 * Construir un arreglo con la capacidad maxima inicial.
@@ -29,87 +29,16 @@ public class ArregloDinamico<T extends Comparable<T>> implements IArregloDinamic
 	 */
 	public ArregloDinamico( int max )
 	{
-		elementos = new String[max];
+		elementos = (T[]) new Comparable[max];
 		tamanoMax = max;
 		tamanoAct = 0;
 	}
 
-	public void agregar( String dato )
-	{
-		if ( tamanoAct == tamanoMax )
-		{  // caso de arreglo lleno (aumentar tamaNo)
-			tamanoMax = 2 * tamanoMax;
-			String [ ] copia = elementos;
-			elementos = new String[tamanoMax];
-			for ( int i = 0; i < tamanoAct; i++)
-			{
-				elementos[i] = copia[i];
-			} 
-			System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
-		}	
-		elementos[tamanoAct] = dato;
-		tamanoAct++;
-	}
 
-	public int darCapacidad() {
-		return tamanoMax;
-	}
 
-	public int darTamano() {
-		return tamanoAct;
-	}
 
-	public String darElemento(int i) {
 
-		String respuesta = "";
-		respuesta+=elementos[i];
-		return respuesta;
-	}
 
-	public String buscar(String dato) {
-		// TODO implementar
-		// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-		String respuesta="el dato a buscar no existe dentro del arreglo dinamico";
-		int i =0;
-		boolean centinel = false;
-		while(i<tamanoAct && !centinel)
-		{
-			if(elementos[i].compareTo(dato)==0)
-			{
-				respuesta= elementos[i]+"el dato se encuentra en la posicion"+i;
-				centinel=true;
-			}
-			i++;
-		}
-		return respuesta;
-
-	}
-
-	public String eliminar(String dato) {
-		// TODO implementar
-		// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-		String buscado="el elemento a eliminar no existe en este arreglo dinamico";
-		int i =0;
-		boolean centinel = false;
-		while(i<tamanoAct && !centinel)
-		{
-			if(elementos[i].compareTo(dato)==0)
-			{
-				buscado= elementos[i];
-				centinel=true;
-				
-			}
-			i++;
-		}
-		while(i<tamanoAct)
-		{
-			elementos[i-1]=elementos[i];
-			i++;
-		}
-		tamanoAct--;
-		return buscado;
-	}
-	
 	public String toString()
 	{
 		String respuesta ="";
@@ -122,4 +51,99 @@ public class ArregloDinamico<T extends Comparable<T>> implements IArregloDinamic
 		return respuesta;
 	}
 
+	@Override
+	public int darCapacidad() {
+		// TODO Auto-generated method stub
+		return tamanoMax;
+	}
+
+	@Override
+	public int darTamano() {
+		// TODO Auto-generated method stub
+		return tamanoAct;
+
+	}
+
+	@Override
+	public void agregar(T dato) {
+		// TODO Auto-generated method stub
+		if ( tamanoAct == tamanoMax )
+		{  // caso de arreglo lleno (aumentar tamaNo)
+			tamanoMax = 2 * tamanoMax;
+			T [ ] copia = elementos;
+			elementos = (T[])new Comparable[tamanoMax];
+			for ( int i = 0; i < tamanoAct; i++)
+			{
+				elementos[i] = copia[i];
+			} 
+			System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
+		}	
+		elementos[tamanoAct] = dato;
+		tamanoAct++;
+
+	}
+
+	@Override
+	public T buscar(T dato) {
+		// TODO Auto-generated method stub
+
+
+		// TODO implementar
+		// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
+		T respuesta=null;
+		int i =0;
+		boolean centinel = false;
+		while(i<tamanoAct && !centinel)
+		{
+			if(elementos[i].compareTo(dato)==0)
+			{
+				respuesta= elementos[i];
+				centinel=true;
+			}
+			i++;
+		}
+		return respuesta;
+
+
+	}
+
+	@Override
+	public T eliminar(T dato) {
+		// TODO Auto-generated method stub
+
+
+		T buscado=null;
+		int i =0;
+		boolean centinel = false;
+		while(i<tamanoAct && !centinel)
+		{
+			if(elementos[i].compareTo(dato)==0)
+			{
+				buscado= elementos[i];
+				centinel=true;
+
+			}
+			i++;
+		}
+		while(i<tamanoAct)
+		{
+			elementos[i-1]=elementos[i];
+			i++;
+		}
+		tamanoAct--;
+		return buscado;
+	}
+
+
+
+
+
+
+	@Override
+	public T darElemento(int i) {
+		 
+		return elementos[i];
+	}
 }
+
+

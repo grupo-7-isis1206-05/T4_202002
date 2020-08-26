4274d1ff93a3c765b91d0300047e3a5a61d7a8bf
AndresCastaneda1 <69652201+AndresCastaneda1@users.noreply.github.com>
2020-08-25 22:53:27 -0500

    test

--- test/test/data_structures/TestArregloDinamico.java
+++ test/test/data_structures/TestArregloDinamico.java
@@ -1,6 +1,8 @@
 package test.data_structures;
 
 import model.data_structures.ArregloDinamico;
+import uniandes.cupi2.centralPacientes.mundo.Paciente;
+import uniandes.cupi2.contactos.mundo.Contacto;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +13,7 @@ public class TestArregloDinamico {
 
 	private ArregloDinamico arreglo;
 	private static int TAMANO=100;
-	
+
 	@Before
 	public void setUp1() {
 		arreglo= new ArregloDinamico(TAMANO);
@@ -24,14 +26,35 @@ public class TestArregloDinamico {
 	}
 
 	@Test
-	public void testArregloDinamico() {
+	public void testArregloDinamico() 
+	{
+		setUp1();
+		assertNull( arreglo.darCapacidad() );
+
 		// TODO
 	}
 
 	@Test
 	public void testDarElemento() {
+
+
 		setUp2();
+		arreglo.darElemento(0);
+
+		assertNotNull( "No se encontró el Elemento" );
+
 		// TODO
 	}
+	public void testBuscar(String dato) {
+		// TODO implementar
+		String c = arreglo.buscar(dato);
+		assertNotNull( c );
+	
+	}
+	public void testEliminar(String dato) {
+		// TODO implementar
+		 arreglo.eliminar(dato);
+			assertNotNull( "Se elimino el elemento con parametro " );
 
-}
+	}
+}  

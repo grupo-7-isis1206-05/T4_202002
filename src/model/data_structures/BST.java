45b315be54835a9521ddf16326e247367b5b6b41
MariaPaulaT <mp.tellez@uniandes.edu.co>
2020-10-14 22:26:14 -0500

    Tabla de simbolos

--- src/model/data_structures/BST.java
+++ src/model/data_structures/BST.java
@@ -10,18 +10,12 @@ public class BST<Key extends Comparable<Key>, Value> {
     private Value[] vals;
     private int n = 0;
 
-    /**
-     * Initializes an empty symbol table.
-     */
+    
     public BST() {
         this(INIT_CAPACITY);
     }
 
-    /**
-     * Initializes an empty symbol table with the specified initial capacity.
-     * @param capacity the maximum capacity
-     */
-    public BST(int capacity) { 
+       public BST(int capacity) { 
         keys = (Key[]) new Comparable[capacity]; 
         vals = (Value[]) new Object[capacity]; 
     }   
@@ -39,11 +33,6 @@ public class BST<Key extends Comparable<Key>, Value> {
         keys = tempk;
     }
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     *
-     * @return the number of key-value pairs in this symbol table
-     */
     public int size() {
         return n;
     }
@@ -59,27 +48,13 @@ public class BST<Key extends Comparable<Key>, Value> {
     }
 
 
-    /**
-     * Does this symbol table contain the given key?
-     *
-     * @param  key the key
-     * @return {@code true} if this symbol table contains {@code key} and
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+   
     public boolean contains(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
     }
 
-    /**
-     * Returns the value associated with the given key in this symbol table.
-     *
-     * @param  key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *         and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    
     public Value get(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to get() is null"); 
         if (isEmpty()) return null;
@@ -88,13 +63,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         return null;
     } 
 
-    /**
-     * Returns the number of keys in this symbol table strictly less than {@code key}.
-     *
-     * @param  key the key
-     * @return the number of keys in the symbol table strictly less than {@code key}
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    
     public int rank(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to rank() is null"); 
 
@@ -111,16 +80,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 
 
 
-    /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old 
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    
     public void put(Key key, Value val)  {
         if (key == null) throw new IllegalArgumentException("first argument to put() is null"); 
 
@@ -137,7 +97,7 @@ public class BST<Key extends Comparable<Key>, Value> {
             return;
         }
 
-        // insert new key-value pair
+        
         if (n == keys.length) resize(2*keys.length);
 
         for (int j = n; j > i; j--)  {
@@ -151,13 +111,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         assert check();
     } 
 
-    /**
-     * Removes the specified key and associated value from this symbol table
-     * (if the key is in the symbol table).
-     *
-     * @param  key the key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+  
     public void delete(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to delete() is null"); 
         if (isEmpty()) return;
@@ -206,16 +160,7 @@ public class BST<Key extends Comparable<Key>, Value> {
     }
 
 
-   /***************************************************************************
-    *  Ordered symbol table methods.
-    ***************************************************************************/
-
-   /**
-     * Returns the smallest key in this symbol table.
-     *
-     * @return the smallest key in this symbol table
-     * @throws NoSuchElementException if this symbol table is empty
-     */
+  
     public Key min() {
         if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
         return keys[0]; 
@@ -232,14 +177,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         return keys[n-1];
     }
 
-    /**
-     * Return the kth smallest key in this symbol table.
-     *
-     * @param  k the order statistic
-     * @return the {@code k}th smallest key in this symbol table
-     * @throws IllegalArgumentException unless {@code k} is between 0 and
-     *        <em>n</em>–1
-     */
+ 
     public Key select(int k) {
         if (k < 0 || k >= size()) {
             throw new IllegalArgumentException("called select() with invalid argument: " + k);
@@ -247,14 +185,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         return keys[k];
     }
 
-    /**
-     * Returns the largest key in this symbol table less than or equal to {@code key}.
-     *
-     * @param  key the key
-     * @return the largest key in this symbol table less than or equal to {@code key}
-     * @throws NoSuchElementException if there is no such key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    
     public Key floor(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to floor() is null"); 
         int i = rank(key);
@@ -263,14 +194,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return keys[i-1];
     }
 
-    /**
-     * Returns the smallest key in this symbol table greater than or equal to {@code key}.
-     *
-     * @param  key the key
-     * @return the smallest key in this symbol table greater than or equal to {@code key}
-     * @throws NoSuchElementException if there is no such key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    
     public Key ceiling(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to ceiling() is null"); 
         int i = rank(key);
@@ -278,16 +202,6 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return keys[i];
     }
 
-    /**
-     * Returns the number of keys in this symbol table in the specified range.
-     *
-     * @param lo minimum endpoint
-     * @param hi maximum endpoint
-     * @return the number of keys in this symbol table between {@code lo} 
-     *         (inclusive) and {@code hi} (inclusive)
-     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
-     *         is {@code null}
-     */
     public int size(Key lo, Key hi) {
         if (lo == null) throw new IllegalArgumentException("first argument to size() is null"); 
         if (hi == null) throw new IllegalArgumentException("second argument to size() is null"); 
@@ -297,28 +211,12 @@ public class BST<Key extends Comparable<Key>, Value> {
         else              return rank(hi) - rank(lo);
     }
 
-    /**
-     * Returns all keys in this symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the foreach notation: {@code for (Key key : st.keys())}.
-     *
-     * @return all keys in this symbol table
-     */
+    
     public Queue<Key> keys() {
         return keys(min(), max());
     }
 
-    /**
-     * Returns all keys in this symbol table in the given range,
-     * as an {@code Iterable}.
-     *
-     * @param lo minimum endpoint
-     * @param hi maximum endpoint
-     * @return all keys in this symbol table between {@code lo} 
-     *         (inclusive) and {@code hi} (inclusive)
-     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
-     *         is {@code null}
-     */
+   
     public Queue<Key> keys(Key lo, Key hi) {
         if (lo == null) throw new IllegalArgumentException("first argument to keys() is null"); 
         if (hi == null) throw new IllegalArgumentException("second argument to keys() is null"); 
